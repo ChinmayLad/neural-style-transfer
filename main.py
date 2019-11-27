1468d8d8ac2fe55584a03ae3a3089bd4e82ff327
ChinmayLad <ladchinmay@gmail.com>
2019-11-27 17:24:31 -0500

    added demo gif

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import torch
+import numpy as np
 from transformer import TransformNetwork
 from image_utils import ImagePipeline
 
@@ -20,7 +21,7 @@ def main():
 	_, frame = capture.read()
 	h, w, c = frame.shape
 	# four_cc = cv2.VideoWriter_fourcc('M','J','P','G')
-	# writer = cv2.VideoWriter('video/output3.avi', four_cc, 10, (w, h))
+	# writer = cv2.VideoWriter('video/output10.avi', four_cc, 10, (w+w-200, h))
 	# writerin = cv2.VideoWriter('video/input.avi', four_cc, 10, (w, h))
 	while capture.isOpened():
 		ret, frame = capture.read()
@@ -28,13 +29,14 @@ def main():
 			if counter%10 == 0:
 				image = pipeline.cvtTensor(frame)
 				image = image.unsqueeze(0)
-				style = transformer(image, 3)
+				style = transformer(image, 5)
 				style = style.squeeze(0)
 				out = pipeline.cvtNumpy(style)
-				# writer.write(out)
-				cv2.imshow('Style', out)
+				out1 = cv2.hconcat([frame,out])
+				# writer.write(out1)
+				cv2.imshow('demo', out1)
 				# writerin.write(frame)
-			cv2.imshow('Original', frame)
+			# cv2.imshow('Original', frame)
 			counter += 1
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
